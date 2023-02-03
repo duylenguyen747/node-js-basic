644a8c55ae5a06905f6a2c6ca6ddf01a20e9f336
duylenguyen747 <duylenguyen747@gmai.com>
2023-02-03 16:50:21 +0700

    first commit

--- sever.js
+++ sever.js
@@ -1,12 +1,11 @@
-const http = require("http");
-const sever = http.createServer((req, res) => {
-  console.log("run request...");
-  res.setHeader("Content-Type", "text/html");
-  res.write("<h3>hello world! </h3>");
-  res.write("<h2>Nguyen le duy</h2>");
-  res.end();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-sever.listen(3000, "localhost", () => {
-  console.log("nodejs sever is running on port: 3000");
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
